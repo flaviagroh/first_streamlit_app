5987562e4121a3c5e24a6de89babd6e4d39f9f6a
Flavia Projects <45563880+flaviagroh@users.noreply.github.com>
2022-04-27 17:02:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas
 
 st.title('My Parents Super New Healthy Dinner')
 st.write('My first app')
 st.write('Hello *world!*')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
